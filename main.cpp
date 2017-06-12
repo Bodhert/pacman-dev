2e1ff520d33679b67a1c5f9dda8769f7e505bd1c
alejo-PC\alejo <alejandro.cordoba.bodhert@gmail.com>
2017-06-11 22:30:56 -0500

    segundo video done

--- main.cpp
+++ main.cpp
@@ -4,6 +4,12 @@
 #define MAXCOLS 31
 BITMAP *buffer;
 BITMAP *roca;
+BITMAP *pacbmp;
+BITMAP *pacman;
+
+int dir = 0;
+int px = 30*10, py = 30*10;
+
 char mapa[MAXFILAS][MAXCOLS] =
 {
     "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
@@ -49,6 +55,26 @@ void pantalla()
     blit(buffer, screen , 0 , 0, 0 , 0, 900, 600);
 }
 
+void dibujar_personaje_abriendo()
+{
+    blit(pacbmp,pacman,dir*33,0,0,0,33,33);
+    draw_sprite(buffer,pacman,px,py);
+}
+
+void dibujar_personaje_cerrando()
+{
+    blit(pacbmp,pacman,4*33,0,0,0,33,33);
+    draw_sprite(buffer,pacman,px,py);
+}
+
+
+void mover_pacman(int dir)
+{
+    if(dir == 0 ) px -= 30;
+    else if(dir == 1) px += 30;
+    else if(dir == 2) py -= 30;
+    else if(dir == 3) py += 30;
+}
 int main ()
 {
     inicia_allegro(900,600);
@@ -56,12 +82,26 @@ int main ()
 
     buffer = create_bitmap(900,600);
     roca = load_bitmap("roca.bmp",NULL);
+    pacbmp = load_bitmap("pacman.bmp",NULL);
+    pacman =  create_bitmap(33,33); // los datos salen de las mediciones de las imagenes
     clear_to_color(buffer, 0x999999);
 
     while(!key[KEY_ESC])
     {
+      if(key[KEY_RIGHT]) dir = 1;
+      else if(key[KEY_LEFT]) dir = 0;
+      else if(key[KEY_UP]) dir = 2;
+      else if(key[KEY_DOWN]) dir = 3;
+      mover_pacman(dir);
+      clear(buffer);
       dibujar_mapa();
+      dibujar_personaje_abriendo();
       pantalla();
+      rest(70);
+      dibujar_personaje_cerrando();
+      pantalla();
+      rest(90);
+
     }
 
 
