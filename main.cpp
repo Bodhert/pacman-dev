727db6178cdf4c859f4672b1a820aea3cf341dba
alejo-PC\alejo <alejandro.cordoba.bodhert@gmail.com>
2017-06-20 15:54:19 -0500

    third video done

--- main.cpp
+++ main.cpp
@@ -6,24 +6,25 @@ BITMAP *buffer;
 BITMAP *roca;
 BITMAP *pacbmp;
 BITMAP *pacman;
+BITMAP *comida;
 
-int dir = 0;
-int px = 30*10, py = 30*10;
+int dir = 4;
+int px = 30*14, py = 30*17;
 
 char mapa[MAXFILAS][MAXCOLS] =
 {
     "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    "X           XXXXX            X",
+    "X      o o  XXXXX  o o       X",
     "X XXX XXXXX XXXXX XXXXX XXX  X",
     "X XXX XXXXX XXXXX XXXXX XXX  X",
     "X                            X",
     "X XXX XX XXXXXXXXXXX XX XXX  X",
     "X     XX     XXX     XX      X",
     "X XXX XXXXXX XXX XXXXXX XXX  X",
-    "X XXX XX             XX XXX  X",
+    "X XXX XX o         o XX XXX  X",
     "      XX XXXXXXXXXXX XX       ",
     "X XXX XX XXXXXXXXXXX XX XXX  X",
-    "X XXX XX             XX XXX  X",
+    "X XXX XX o         o XX XXX  X",
     "X XXX XXXXXX XXX XXXXXX XXX  X",
     "X     XX     XXX     XX      X",
     "X XXX XX XXXXXXXXXXX XX XXX  X",
@@ -46,6 +47,11 @@ void dibujar_mapa()
             {
                 draw_sprite(buffer, roca , col*30, row*30 );  // el *30 sale del tamaño de la imagen
             }
+            else if(mapa[row][col] == 'o')
+            {
+                draw_sprite(buffer, comida , col*30, row*30 );
+                if(py/30 == row && px/30 == col) mapa[row][col] = ' ';
+            }
         }
     }
 }
@@ -67,13 +73,47 @@ void dibujar_personaje_cerrando()
     draw_sprite(buffer,pacman,px,py);
 }
 
+bool game_over()
+{
+    int row, col;
+     for(row = 0 ; row < MAXFILAS; ++row)
+        for(col = 0; col < MAXCOLS; ++col)
+             if(mapa[row][col] == 'o') return true;
+    return false;
+}
+
 
 void mover_pacman(int dir)
 {
-    if(dir == 0 ) px -= 30;
-    else if(dir == 1) px += 30;
-    else if(dir == 2) py -= 30;
-    else if(dir == 3) py += 30;
+    if(dir == 0  )
+    {
+        if(mapa[py/30][(px-30)/30] != 'X') px -= 30;
+        else dir = 4;
+    }
+    else if(dir == 1)
+    {
+        if(mapa[py/30][(px+30)/30] != 'X') px += 30;
+        else dir = 4;
+    }
+    else if(dir == 2)
+    {
+        if(mapa[(py-30)/30][px/30] != 'X') py -= 30;
+        else dir = 4;
+
+    }
+    else if(dir == 3)
+    {
+        if(mapa[(py+30)/30][px/30] != 'X') py += 30;
+        else dir =  4;
+    }
+
+    //rutina para atajos
+
+    if( px <= -30) px = 870;
+    else if (px >= 875) px = -30;
+
+
+
 }
 int main ()
 {
@@ -84,9 +124,10 @@ int main ()
     roca = load_bitmap("roca.bmp",NULL);
     pacbmp = load_bitmap("pacman.bmp",NULL);
     pacman =  create_bitmap(33,33); // los datos salen de las mediciones de las imagenes
+    comida = load_bitmap("Comida.bmp", NULL);
     clear_to_color(buffer, 0x999999);
 
-    while(!key[KEY_ESC])
+    while(!key[KEY_ESC] && game_over())
     {
       if(key[KEY_RIGHT]) dir = 1;
       else if(key[KEY_LEFT]) dir = 0;
